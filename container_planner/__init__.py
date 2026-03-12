8f40faf6319991d29d517d98734d0da1f5d818d9
kyo563 <219984293+kyo563@users.noreply.github.com>
2026-03-12 18:08:15 +0900

    Add container KPI aggregation and exports

--- container_planner/__init__.py
+++ container_planner/__init__.py
@@ -2,7 +2,7 @@ from container_planner.io import CargoInputError, expand_pieces, load_cargo_csv,
 from container_planner.naccs import load_package_master, map_package_text
 from container_planner.oog import evaluate_oog
 from container_planner.planner import estimate, validate
-from container_planner.reporting import build_placement_rows
+from container_planner.reporting import build_container_kpi_rows, build_placement_rows
 
 __all__ = [
     "CargoInputError",
@@ -16,4 +16,5 @@ __all__ = [
     "estimate",
     "validate",
     "build_placement_rows",
+    "build_container_kpi_rows",
 ]
